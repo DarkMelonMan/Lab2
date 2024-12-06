d76b0af6bd9eb735b0f4e4c5c0ac0caa9d559e6c
DarkMelonMan <77286390+DarkMelonMan@users.noreply.github.com>
2024-12-06 18:34:42 +0700

    Update Lab2.cpp

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -1,7 +1,27 @@
-﻿#include "Structures.cpp"
+#include "Structures.cpp"
 #include "Classes.h"
 
 int main()
 {
-	DamageTest::ChooseTestType();
+	do {
+		cout << endl << "Choose test type:" << endl << "1)Structures" << endl <<
+			"2)Classes" << endl << "3)Exit" << endl;
+		do {
+			cin >> DamageTest::testChoice;
+			if (DamageTest::testChoice < 1 || DamageTest::testChoice > 3)
+				cout << "Wrong choice. Try again: " << endl;
+		} while (DamageTest::testChoice < 1 || DamageTest::testChoice > 3);
+		if (DamageTest::testChoice == 3)
+			exit(0);
+		switch (DamageTest::testChoice) {
+		case 1:
+			DamageTest::ChooseStruct();
+			break;
+		case 2:
+			DamageTest::ChooseClass();
+			break;
+		default:
+			break;
+		}
+	} while (DamageTest::testChoice != 3);
 }
