9adbdea0ef2801f3b8facb20c135a0b292b6a90e
DarkMelonMan <77286390+DarkMelonMan@users.noreply.github.com>
2024-12-18 18:45:49 +0700

    Добавлены блоки try-catch

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -6,22 +6,27 @@ int main()
 	do {
 		cout << endl << "Choose test type:" << endl << "1)Structures" << endl <<
 			"2)Classes" << endl << "3)Exit" << endl;
-		do {
-			cin >> DamageTest::testChoice;
-			if (DamageTest::testChoice < 1 || DamageTest::testChoice > 3)
-				cout << "Wrong choice. Try again: " << endl;
-		} while (DamageTest::testChoice < 1 || DamageTest::testChoice > 3);
-		if (DamageTest::testChoice == 3)
-			exit(0);
-		switch (DamageTest::testChoice) {
-		case 1:
-			DamageTest::ChooseStruct();
-			break;
-		case 2:
-			DamageTest::ChooseClass();
-			break;
-		default:
-			break;
+		try {
+			do {
+				cin >> DamageTest::testChoice;
+				if (DamageTest::testChoice < 1 || DamageTest::testChoice > 3)
+					cout << "Wrong choice. Try again: " << endl;
+			} while (DamageTest::testChoice < 1 || DamageTest::testChoice > 3);
+			if (DamageTest::testChoice == 3)
+				exit(0);
+			switch (DamageTest::testChoice) {
+			case 1:
+				DamageTest::ChooseStruct();
+				break;
+			case 2:
+				DamageTest::ChooseClass();
+				break;
+			default:
+				break;
+			}
+		}
+		catch (const TestError& e) {
+			cout << endl << "Test was unsuccessful" << endl;
 		}
 	} while (DamageTest::testChoice != 3);
 }
